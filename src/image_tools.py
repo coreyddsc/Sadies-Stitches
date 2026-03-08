c4804dee32e740b806e099df14b73538b3aede3d
coreyddsc <coreyd.dsc@gmail.com>
2026-03-07 19:11:04 -0500

    aspect ratio classifications

--- src/image_tools.py
+++ src/image_tools.py
@@ -36,3 +36,36 @@ def convert_jpg2png(dir: Path, unlink: bool = False):
 			print(f"Failed to read {jpg_path.name}")
 
 
+def get_aspect_ratio(image_path: Path):
+    img = cv2.imread(str(image_path))
+    if img is None:
+        return None
+    
+    height, width = img.shape[:2]
+    aspect_ratio = width / height
+    return aspect_ratio
+
+def classify_aspect_ratio(aspect: float):
+	if aspect > 1.2:  # Wider than tall
+		classification = "landscape"
+	elif aspect < 0.8:  # Taller than wide
+		classification = "portrait"
+	else:  # Roughly square
+		classification = "square"
+	return classification
+
+def classify_by_aspect_ratio(dir: Path):
+	if isinstance(dir, Path):
+		pass
+	else:
+		dir = Path(dir)
+	img_dir = dir / "images" / "gallery"
+	imgs = get_image_names(dir)
+	img_classifier = {}
+	for img in imgs:
+		img_path = img_dir / img
+		aspect_ratio = get_aspect_ratio(img_path)
+		classification = classify_aspect_ratio(aspect_ratio)
+		img_classifier.setdefault(img, (aspect_ratio, classification))
+
+	return img_classifier
--- tests/test_image_tools.py
+++ tests/test_image_tools.py
@@ -6,7 +6,7 @@ from src.image_tools import *
 class BaseCaseTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.dir = os.getcwd()
+        cls.dir = Path(os.getcwd())
         
 
 class TestGetImageNames(BaseCaseTest):
@@ -18,3 +18,18 @@ class TestGetImageNames(BaseCaseTest):
 class TestConvertJPG2PNG(BaseCaseTest):
     def test_convert_jpg2png(self):
         convert_jpg2png(dir = self.dir, unlink=True)
+        
+        
+class TestGetAspectRatio(BaseCaseTest):
+    def test_get_aspect_ratio(self):
+        imgs = get_image_names(self.dir)
+        img_dir = self.dir / "images" / "gallery" / imgs[0]
+        aspect_ratio = get_aspect_ratio(img_dir)
+        print(f"Test Image Aspect Ratio: {aspect_ratio}")
+        
+        
+class TestClassifyByAspectRatio(BaseCaseTest):
+    def test_classify_by_aspect_ratio(self):
+        img_class = classify_by_aspect_ratio(self.dir)
+        for k, v in img_class.items():
+            print(f"Image Name: {k} | Aspect Ratio: {v[0]} | Classification: {v[1]}")
